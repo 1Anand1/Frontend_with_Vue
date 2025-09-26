f5207cb7debd4fd3e77b6213525a524633b976f3
1Anand1 <anand22dandavate@gmail.com>
2025-09-26 22:21:52 +0530

    Added few more conditions.

--- vue_demos/src/App.vue
+++ vue_demos/src/App.vue
@@ -1,13 +1,14 @@
 <script>
 export default {
   data() {
-    return { name: " Anand Dandavate", status: false };
+    return { name: " Anand Dandavate", status: "pending" };
   },
 };
 </script>
 
 <template>
   <h1>{{ name }}</h1>
-  <p v-if="status">User is active</p>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 </template>
